daf93f970470781b6f237f6ae64cd306a0465a21
DESCELIA YOSHE <TP049158@mail.apu.edu.my>
2021-07-28 22:56:30 +0700

    .

---
+++ ddacassignment/Areas/Identity/Data/ddacassignmentContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ddacassignment.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ddacassignment.Data
+{
+    public class ddacassignmentContext : IdentityDbContext<ddacassignmentUser>
+    {
+        public ddacassignmentContext(DbContextOptions<ddacassignmentContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ddacassignment/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ddacassignment.Areas.Identity.Data;
+using ddacassignment.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ddacassignment.Areas.Identity.IdentityHostingStartup))]
+namespace ddacassignment.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ddacassignmentContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ddacassignmentContextConnection")));
+
+                services.AddDefaultIdentity<ddacassignmentUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ddacassignmentContext>();
+            });
+        }
+    }
+}
